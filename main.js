290844d897b23dd77985440e013440059f2066a5
mmunjal <mohit.munjal@amdocs.com>
2019-07-27 20:35:39 -0500

    Initial commit

--- main.js
+++ main.js
@@ -76,27 +76,122 @@ function loadSlide1() {
 
 
 function loadSlide2() {
-	var svgBar = d3.select("#slide2 #svg-custom");
-	var countries = [];
-	var w = 500;
-	var h = 100;
-	var barPadding = 3;
-	d3.csv("data/world_population.csv",
-            function(error, rows) {
-                rows.forEach(function(r) {
-                    countries.push({
-                        name: r.Country,
-                        values: [r['2001'], r['2002'], r['2003'], r['2004']]
-                    })
-                });
-                countries.forEach(function(b){
-                    render(b);
-                });
-            });
+	
+
+	
+	var svg = d3.select("#slide2 #svg-custom")
+	     margin = 200, width = svg.attr("width") - margin,
+         height = svg.attr("height") - margin;
+         
+         svg.append("text")
+            .attr("transform", "translate(100,0)")
+            .attr("x", 50).attr("y", 50)
+            .attr("font-size", "20px")
+            .attr("class", "title")
+            .text("Population bar chart")
+            
+         var x = d3.scaleBand().range([0, width]).padding(0.4),
+         y = d3.scaleLinear().range([height, 0]);
+            
+         var g = svg.append("g")
+            .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+         d3.csv("data/world_population1.csv", function(error, data) {
+            if (error) {
+               throw error;
+            }
+               
+            x.domain(data.map(function(d) { return d.year; }));
+            y.domain([0, d3.max(data, function(d) { return d.population; })]);
+                     
+            g.append("g")
+               .attr("transform", "translate(0," + height + ")")
+               .call(d3.axisBottom(x))
+				.selectAll("text")
+				.attr("y", 0)
+				.attr("x", 9)
+				.attr("dy", ".35em")
+				.attr("transform", "rotate(90)")
+				.style("text-anchor", "start");
+						   
+            g.append("g")
+               .append("text")
+               .attr("transform", "rotate(-90)")
+               .attr("y", 6)
+               .attr("dy", "-5.1em")
+               .attr("text-anchor", "end")
+               .attr("font-size", "18px")
+               .attr("stroke", "blue");
+               
+                         
+            g.append("g")
+               .attr("transform", "translate(0, 0)")
+               .call(d3.axisLeft(y))
+
+            g.selectAll(".bar")
+               .data(data)
+               .enter()
+               .append("rect")
+               .attr("class", "bar")
+               .on("mouseover", onMouseOver) 
+               .on("mouseout", onMouseOut)   
+               .attr("x", function(d) { return x(d.year); })
+               .attr("y", function(d) { return y(d.population); })
+               .attr("width", x.bandwidth()).transition()
+               .ease(d3.easeLinear).duration(800)
+               .delay(function (d, i) {
+                  return i * 25;
+               })
+                  
+            .attr("height", function(d) { return height - y(d.population); });
+         });
+          
+
+
+
 }
+function onMouseOver(d, i) {
+   d3.select(this)
+   .attr('class', 'highlight');
+      
+   d3.select(this)
+      .transition()     
+      .duration(200)
+      .attr('width', x.bandwidth() + 5)
+      .attr("y", function(d) { return y(d.population) - 10; })
+      .attr("height", function(d) { return height - y(d.population) + 10; });
+     
+   g.append("text")
+      .attr('class', 'val')
+      .attr('x', function() {
+         return x(d.year);
+      })
+      
+   .attr('y', function() {
+      return y(d.value) - 10;
+   })
+}
+ 
+function onMouseOut(d, i) {
+    
+   d3.select(this)
+      .attr('class', 'bar');
+   
+   d3.select(this)
+      .transition()     
+      .duration(200)
+      .attr('width', x.bandwidth())
+      .attr("y", function(d) { return y(d.population); })
+      .attr("height", function(d) { return height - y(d.population); });
+   
+   d3.selectAll('.val')
+      .remove()
+}
+function type(d) {
+  d.population = +d.population;
+  return d;
 
-
-
+}
 
 function loadViz() {
 	loadSlide1();
---
+++ main1.js
@@ -0,0 +1,217 @@
+var projection = d3.geoMercator();
+var data = null;
+
+var qualityOfLifeColors = null;
+
+function getDomain(prop) {
+	var scale = _.map(data.features, function(node) {
+		if (node.properties[prop]) return node.properties[prop];
+		else return null;
+	});
+	scale = _.uniq(scale);
+	 return d3.extent(scale);
+}
+
+function setScales() {
+	qualityOfLifeColors = d3.scaleLinear().domain(getDomain('Quality of Life Index')).range(['red','green']);
+}
+
+function loadData(csv, json) {
+	var countries = json;
+	var csvData = _.indexBy(d3.csvParse(csv), 'Country');
+	countries.features = _.map(countries.features, function(node) {
+		if (csvData[node.properties.name]) {
+			var countryData = csvData[node.properties.name];
+			for (var k in countryData) {
+				if (k != 'Country') countryData[k] = countryData[k]*1;
+			}
+			node.properties = countryData;
+		} else {
+			node.properties = {Country: node.properties.name}
+		}
+		return node;
+	});
+
+	return countries;
+}
+
+function loadSlide1() {
+	var svg = d3.select("#slide1 svg");
+
+	var path = d3.geoPath()
+		.projection(projection);
+
+	var g = svg.append("g");
+
+	g.selectAll("path")
+		.data(data.features)
+		.enter()
+		.append("path")
+		.attr("d", path)
+		.attr("class", "country")
+		.style("fill-opacity", 0.7)
+		.style("fill", function(data){
+			if (data.properties['Quality of Life Index']) {
+				return qualityOfLifeColors(data.properties['Quality of Life Index']);
+			}
+			else return "lightgrey";
+		}).on('mouseover', function(d, i){
+			d3.select(this).style('fill-opacity', 1);
+			$('#map-help').hide();
+			$('#country-name').text(d.properties['Country']);
+			var prop = $('#country-properties').empty();
+			_.each(d.properties, function(v, k) {
+				prop.append("<li><b>"+k+":</b> " + v + "</li>")
+			});
+		}).on('mouseout', function(d, i) {
+			g.selectAll("path").style('fill-opacity', 0.7);
+			$('#country-name').empty();
+			$('#country-properties').empty();
+			$('#map-help').show();
+		});
+
+}
+
+
+function loadSlide2() {
+	var svgMap = d3.select("#slide2 #svg-custom");
+
+	$("#svg-prop-bar").empty();
+	var svgBar = d3.select("#slide2 #svg-prop-bar");
+
+	var tip = d3.select("body").append("div")	
+	    .attr("class", "tooltip")				
+	    .style("opacity", 0);
+
+	var index = $('#custom-index').val();
+	var order = $('#custom-index option:selected').attr('data-order') == 'desc' ? 1 : -1;
+	var indexDomain = getDomain(index);
+	if (order == -1) indexDomain.reverse();
+	var indexDomain = d3.scaleLinear().domain(indexDomain);
+
+	// Get countries in order of index
+	data.features.sort(function(a, b) {
+		if (a.properties[index] && b.properties[index]) {
+			if (a.properties[index] < b.properties[index]) return 1 * order;
+			if (a.properties[index] > b.properties[index]) return -1 * order;
+			return 0;
+		}
+		if (a.properties[index]) return -1;
+		if (b.properties[index]) return 1;
+		return 0;
+	});
+
+	// Draw the map
+	var indexColors = indexDomain.range(['red','green']);
+
+	var path = d3.geoPath()
+		.projection(projection);
+
+	var g = svgMap.append("g");
+
+	g.selectAll("path")
+		.data(data.features)
+		.enter()
+		.append("path")
+		.attr("d", path)
+		.attr("class", "country")
+		.style("fill-opacity", 0.7)
+		.style("fill", function(d, i){
+			if (d.properties[index]) {
+				return indexColors(d.properties[index]);
+			}
+			else return "lightgrey";
+		})
+		.on('mouseover', function(d,i) {
+			d3.select(this).style('fill-opacity', 1);
+			var propText = ['<table>'];
+			_.each(d.properties, function(v, k) {
+				propText.push('<tr><td>' + k.replace('Index', '').trim() + "</td><td>" + v + "</td></tr>");
+			});
+			propText.push('</table>');
+			tip.transition()		
+                .duration(200)		
+                .style("opacity", .9);
+            tip	.html(propText.join(''))
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");
+		})
+		.on('mousemove', function(d, i){
+			tip.style("left", (d3.event.pageX) + "px")
+		       .style("top", (d3.event.pageY - 28) + "px")			
+		})
+		.on('mouseout', function(d,i) {
+			g.selectAll("path").style('fill-opacity', 0.7);
+			tip.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+		})
+
+		// Draw the bar chart for selected index
+		var topData = data.features.slice(0, 20);
+
+		var barHeight = d3.scaleLinear().domain([topData[0].properties[index], topData[19].properties[index]]).range([300,10]);
+
+		var barWidth = $('#svg-prop-bar').width() / topData.length;
+
+		var height = $('#svg-prop-bar').height();
+
+
+		var bar = svgBar.selectAll("g")
+					.data(topData)
+					.enter().append("g")
+					.attr("transform", function(d, i) { return "translate("+i*barWidth+",0)"})
+					.append("rect")
+					.attr("y", function(d, i) { return  height - barHeight(d.properties[index]); })
+					.attr("height", function(d, i) { return barHeight(d.properties[index]); })
+					.attr("width", barWidth - 1)
+					.style("fill", function(d, i){
+						return indexColors(d.properties[index]);
+					}).on('mouseover', function(d,i) {
+						var propText = ['<table>'];
+						var country = d.properties.Country;
+						var indexData = d.properties[index];
+						propText.push('<tr><td>Country: ' + "</td><td>" + country + "</td></tr>");
+						propText.push('<tr><td>' + index.replace('Index','') + ': ' + "</td><td>" + indexData + "</td></tr>");
+						_.each(d.properties, function(v, k) {
+							
+						});
+						propText.push('</table>');
+						tip.transition()		
+			                .duration(200)		
+			                .style("opacity", .9);
+			            tip	.html(propText.join(''))
+			                .style("left", (d3.event.pageX) + "px")		
+			                .style("top", (d3.event.pageY - 28) + "px");
+					})
+					.on('mousemove', function(d, i){
+						tip.style("left", (d3.event.pageX) + "px")
+					       .style("top", (d3.event.pageY - 28) + "px")			
+					})
+					.on('mouseout', function(d,i) {
+						tip.transition()		
+			                .duration(500)		
+			                .style("opacity", 0);	
+					})
+
+}
+
+function loadViz() {
+
+	$('#custom-index').change(function(){
+		loadSlide2();
+	});
+	loadSlide2();
+}
+
+$(function(){
+	$.when($.get('data/quality-of-life.csv'), $.getJSON('data/countries.geo.json'))
+		.then(function(v1, v2) {
+			var csv = v1[0];
+			var json = v2[0];
+			data = loadData(csv, json);
+			setScales();
+			loadViz();
+		});
+});
+
